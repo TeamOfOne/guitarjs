dd55649edce4eb04508866c566eb85c82a33af37
Fernando Nogueira <fjsnogueira@gmail.com>
2016-09-10 14:44:16 +0100

    exporting to png

--- wwwroot/chords/chords.controller.js
+++ wwwroot/chords/chords.controller.js
@@ -140,6 +140,47 @@
             addChord();
         }
 
+        vm.exportToPng = exportToPng;
+
+        function exportToPng(idx) {
+            var target = "chord_" + idx;
+
+            svg_to_png(target);
+        }
+
+        //from: https://gist.github.com/gustavohenke/9073132
+        function svg_to_png(container) {
+
+            console.log(container);
+
+            var svg = document.getElementById(container);
+
+            if (typeof window.XMLSerializer != "undefined") {
+                var svgData = (new XMLSerializer()).serializeToString(svg);
+            } else if (typeof svg.xml != "undefined") {
+                var svgData = svg.xml;
+            }
+
+            var canvas = document.createElement("canvas");
+            var svgSize = svg.getBoundingClientRect();
+            canvas.width = svgSize.width;
+            canvas.height = svgSize.height;
+            var ctx = canvas.getContext("2d");
+
+            var img = document.createElement("img");
+            img.setAttribute("src", "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgData))) );
+
+            img.onload = function() {
+                ctx.drawImage(img, 0, 0);
+                var imgsrc = canvas.toDataURL("image/png");
+
+                var a = document.createElement("a");
+                a.download = container+".png";
+                a.href = imgsrc;
+                a.click();
+            };
+        }
+
         ngOnInit();
     }
 })();
