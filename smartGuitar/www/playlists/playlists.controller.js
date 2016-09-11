1a94507d88aed91dac233c80c8084b520975dc7e
Fernando Nogueira <fjsnogueira@gmail.com>
2016-09-11 12:02:29 +0100

    added playlists controllers, and locker menu

---
+++ smartGuitar/www/playlists/playlists.controller.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    angular.module('app')
+        .controller('PlaylistsCtrl', PlaylistsCtrl);
+
+    PlaylistsCtrl.$inject = ["_", "$rootScope", $scope];
+    
+    function PlaylistsCtrl(_, $rootScope, $scope) {
+                
+        $scope.playlists = [
+            { title: 'Reggae', id: 1 },
+            { title: 'Chill', id: 2 },
+            { title: 'Dubstep', id: 3 },
+            { title: 'Indie', id: 4 },
+            { title: 'Rap', id: 5 },
+            { title: 'Cowbell', id: 6 }
+        ];
+    }
+})();
