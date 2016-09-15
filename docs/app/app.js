a966085cc26fe0af418eb7a8b180685459e602d6
Fernando Nogueira <fjsnogueira@gmail.com>
2016-09-15 18:06:52 +0100

    added static app hosting via docs folder

---
+++ docs/app/app.js
@@ -0,0 +1,25 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'app' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'app.controllers' is found in controllers.js
+angular.module('app', ['ionic', 'app.controllers'])
+.constant('_', window._)
+ //use in views, ng-repeat="x in _.range(3)"
+.run(function($ionicPlatform, $rootScope) {
+  $rootScope._ = window._
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+});
---
+++ docs/app/app.routes.js
@@ -0,0 +1,68 @@
+(function () {
+    'use strict';
+
+    angular.module('app')
+        .config(defineRoutes);
+
+    defineRoutes.$inject = ["$stateProvider", "$urlRouterProvider"];
+
+    function defineRoutes ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
+            .state('app.search', {
+                url: '/search',
+                views: {
+                'menuContent': {
+                    templateUrl: 'templates/search.html'
+                }
+                }
+            })
+            .state('app.browse', {
+                url: '/browse',
+                views: {
+                    'menuContent': {
+                    templateUrl: 'templates/browse.html'
+                    }
+                }
+                })
+            .state('app.playlists', {
+                url: '/playlists',
+                views: {
+                    'menuContent': {
+                    templateUrl: 'playlists/playlists.html',
+                    controller: 'PlaylistsCtrl'
+                    }
+                }
+                })
+
+            .state('app.chords', {
+                url: '/playlists/:chordId/:chordname',
+                views: {
+                        'menuContent': {
+                        templateUrl: 'chords/chords.html',
+                        controller: 'ChordsController',
+                        controllerAs: 'chordsCtrl'
+                        }
+                    }
+                })
+
+            .state('app.single', {
+                url: '/playlists/:playlistId',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'playlist/playlist.html',
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            })
+            
+            ;
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/playlists');
+    }
+})();
